3bc654609fd493ad55fa98e1c50f04171e8720a5
Adam Markon <amarkon@hubspot.com>
2019-12-15 21:43:20 -0500

    add custom build target,use xbuild to compile core
    not a ton of code here, but a fair amount got done. set up the target
    image to build for bare metal instead of building for linux, used
    xbuild so that we recompile the core libraries for this new environment
    instead of using the linux-compiled binaries, and updated github
    build to use the new target. also set up a cargo config so we don't
    need to specify the target on every build command

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,26 @@ use core::panic::PanicInfo;
 // Called on panic, global exception handler
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    // just loop infinitely for now, no need to do anything
+    // interesting here yet
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
+// rust mangles function names by default, can't do
+// that or else linux bindings won't work, system expects _start
 #[no_mangle]
+// linker looks for this function name, explicitly delcare that
+// we're exposing a C binding
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
