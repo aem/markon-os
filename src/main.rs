1cb32c19c79ccf22d15582dfc066d4f99b63e450
Adam Markon <amarkon@hubspot.com>
2019-12-18 19:33:47 -0500

    create custom test runner and conditionally run
    because we're not including the std library, it means that we also don't
    get access to the built-in test runner :roll_eyes: fortunately, we can
    add some compiler directives to use a custom test runner, define a
    custom test-main function, and then conditionally execute it depending
    on whether or not we're in a test build or not. i'm going to want to do
    a little metaprogramming to make the progress report print
    automatically, maybe print out some stats (runtime, %success, etc), and
    i'm not sure what else.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 use core::panic::PanicInfo;
 mod vga_buffer;
 
@@ -10,15 +14,41 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
-// rust mangles function names by default, can't do
-// that or else linux bindings won't work, system expects _start
-#[no_mangle]
-// linker looks for this function name, explicitly delcare that
-// we're exposing a C binding
-pub extern "C" fn _start() -> ! {
+fn print_sample_vga_text() {
     for i in 0x21..0x7e {
         print!("{}", i as u8 as char);
     }
     println!("The numbers are {} and {}", 41, 1.0 / 3.0);
     panic!("ah shit it's all borked now innit");
+}
+
+// rust mangles function names by default, can't do
+// that or else linux bindings won't work, system expects _start
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    // linker looks for this function name, explicitly delcare that
+    // we're exposing a C binding
+
+    #[cfg(not(test))]
+    print_sample_vga_text();
+
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} test(s)", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("Trivial assertion...");
+    assert_eq!(1, 1);
+    print!("[OK]")
 }
