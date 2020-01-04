6c8ec59266ba942f61938545a94e167aef8e86c9
Adam Markon <amarkon@hubspot.com>
2020-01-03 19:02:38 -0500

    move OS into long mode, allowing 64-bit instructions

--- src/x86_64/compile.sh
+++ src/x86_64/compile.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 printf "%s\n\n" "Cleaning any old build output. Removing:"
-rm -f -v boot boot.o multiboot_header multiboot_header.o kernel.bin
+rm -f -v boot boot.o multiboot_header multiboot_header.o kernel.bin long_mode_init.o long_mode_init
 printf "\n%s\n" "Files removed. Compiling assembly..."
 nasm -f elf64 multiboot_header.asm
 nasm -f elf64 boot.asm
+nasm -f elf64 long_mode_init.asm
 printf "%s\n" "Linking binary..."
-ld -n -o kernel.bin -T linker.ld multiboot_header.o boot.o
+ld -n -o kernel.bin -T linker.ld multiboot_header.o boot.o long_mode_init.o
 printf "%s\n" "Building ISO..."
 mkdir -p isofiles/boot/grub
 cp ./grub/grub.cfg isofiles/boot/grub/grub.cfg
